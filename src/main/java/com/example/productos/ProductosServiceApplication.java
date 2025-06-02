d04c133a093ac06f12f32b8c5deb363c56490137
Jaime Barraza <jai.barraza@duocuc.cl>
2025-06-02 18:19:54 -0400

    Modificacion del ComponentScan

--- src/main/java/com/example/productos/ProductosServiceApplication.java
+++ src/main/java/com/example/productos/ProductosServiceApplication.java
@@ -2,10 +2,10 @@ package com.example.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.example.productos","com.example.boletas"})
+@SpringBootApplication(scanBasePackages = {"com.example.productos", "com.example.boletas"})
+//@ComponentScan(basePackages = {"com.example.productos","com.example.boletas"})
 public class ProductosServiceApplication {
 
 	public static void main(String[] args) {
