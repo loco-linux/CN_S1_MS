c6fbb1b1fedf2f0ee4c1d692547934dd01e44e45
Jaime Barraza <jai.barraza@duocuc.cl>
2025-06-02 18:13:58 -0400

    Ampliacion de ComponentScan para que el index vea 'boletas'

--- src/main/java/com/example/productos/ProductosServiceApplication.java
+++ src/main/java/com/example/productos/ProductosServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.productos","com.example.boletas"})
 public class ProductosServiceApplication {
 
 	public static void main(String[] args) {
