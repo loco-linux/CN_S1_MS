0a725032b213725a59f2c5ddf6c264ad911e7d45
Jaime Barraza <jai.barraza@duocuc.cl>
2025-06-02 22:51:09 -0400

    Se agregan Endpoints REST y se agrega Swagger

--- src/main/java/com/example/tienda/boletas/controller/BoletaController.java
+++ src/main/java/com/example/tienda/boletas/controller/BoletaController.java
@@ -1,6 +1,10 @@
 package com.example.tienda.boletas.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.tienda.boletas.dto.CarritoCompraRequest;
 import com.example.tienda.boletas.model.Boleta;
 import com.example.tienda.boletas.service.BoletaService;
+
 
 @RestController
 @RequestMapping("/api/v1/boletas")
@@ -26,8 +31,23 @@ public class BoletaController {
         return boletaService.crearBoleta(request);
     }
 
-    // GET de prueba para verificar que BoletaController responde
     @GetMapping
+    public List <Boleta> listarBoletas(){
+        return boletaService.listarTodas();
+    }
+    
+    @GetMapping("/{id}")
+    public Boleta obtenerBoleta(@PathVariable Long id){
+        return boletaService.buscarPorId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarBoleta(@PathVariable Long id){
+        boletaService.eliminarPorId(id);
+    }
+
+    // GET de prueba para verificar que BoletaController responde
+    @GetMapping("/ping")
     public String mensajePrueba() {
         return "Boletas GET";
     }
--- src/main/java/com/example/tienda/boletas/service/BoletaService.java
+++ src/main/java/com/example/tienda/boletas/service/BoletaService.java
@@ -3,7 +3,6 @@ package com.example.tienda.boletas.service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -17,11 +16,11 @@ import com.example.tienda.productos.service.ProductService;
 public class BoletaService {
 
     private final BoletaRepository boletaRepository;
-    private final ProductService productoService; // Ya lo tienes
+    private final ProductService productService;
 
-    public BoletaService(BoletaRepository boletaRepository, ProductService productoService) {
+    public BoletaService(BoletaRepository boletaRepository, ProductService productService) {
         this.boletaRepository = boletaRepository;
-        this.productoService = productoService;
+        this.productService = productService;
     }
 
     public Boleta crearBoleta(CarritoCompraRequest request) {
@@ -29,15 +28,14 @@ public class BoletaService {
         boleta.setFecha(LocalDateTime.now());
 
         List<DetalleBoleta> detalles = request.getProductos().stream().map(item -> {
-            var producto = productoService.findProductoById(item.getProductoId());
+            var producto = productService.findProductoById(item.getProductoId());
             DetalleBoleta detalle = new DetalleBoleta();
             detalle.setProductoId(producto.getId());
             detalle.setCantidad(item.getCantidad());
-            detalle.setPrecioUnitario(BigDecimal.valueOf(producto.getPrecio())); 
+            detalle.setPrecioUnitario(BigDecimal.valueOf(producto.getPrecio()));
             detalle.setBoleta(boleta);
             return detalle;
-        }).collect(Collectors.toList());
-        
+        }).toList();
 
         boleta.setDetalles(detalles);
 
@@ -48,5 +46,18 @@ public class BoletaService {
         boleta.setTotal(total);
 
         return boletaRepository.save(boleta);
+    }
+
+    public List<Boleta> listarTodas() {
+        return boletaRepository.findAll();
+    }
+
+    public Boleta buscarPorId(Long id) {
+        return boletaRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Boleta no encontrada con ID: " + id));
+    }
+
+    public void eliminarPorId(Long id) {
+        boletaRepository.deleteById(id);
     }
 }
