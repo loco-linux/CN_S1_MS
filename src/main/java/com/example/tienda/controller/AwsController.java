cdc3b589a6e024f4bee0e52ab62a54db432ff116
Jaime Barraza <jai.barraza@duocuc.cl>
2025-06-06 21:47:39 -0400

    Integracion AWS

--- src/main/java/com/example/tienda/controller/AwsController.java
+++ src/main/java/com/example/tienda/controller/AwsController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping; // Asumo que es PutMapping por el contexto, la imagen no es clara.
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
